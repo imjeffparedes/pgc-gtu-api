b84e58c7b45fa7774f0b4252bdd773180f07216c
Jefferson Paredes <jeff@comgtech.com>
2019-08-12 21:19:17 +0800

    Initial commit

---
+++ example/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const GTU = require("../lib");
+
+var client = new GTU({
+    publicKey: process.env.publicKey
+  , privateKey: process.env.privateKey
+  , dealerCode: process.env.dealerCode
+    // This is optional
+  , host: process.env.HOST
+  , port: process.env.PORT
+  , timeout: process.env.TIMEOUT
+});
+
+var requestId = parseInt(Math.random() * (1000000 - 1000) + 1000);
+
+client.balanceInquiry(requestId, (err, data) => {
+    console.log(err || data);
+    // =>
+    // {
+    //     "status": "COMPLETE",
+    //     "message": "SUCCESS",
+    //     "balance": 0,
+    // }
+});
